3bd9f92b6f361a6393a3f4f7039d1bfe5e1253b6
rm <222302495+john-rm@users.noreply.github.com>
2026-05-29 15:44:51 +0000

    cleaned up crc

--- opendbc/car/c/crc.h
+++ opendbc/car/c/crc.h
@@ -1,12 +1,28 @@
-#include "crc.h"
-#include <stdlib.h>
+#ifndef CRC_HDR
+#define CRC_HDR
 
-uint8_t CRC8H2F[256];
-uint8_t CRC8J1850[256];
-uint8_t CRC8BODY[256];
-uint16_t CRC16_XMODEM[256];
+#include <stdint.h>
+#include <stddef.h>
 
-static void gen_crc8_table(uint8_t* table, int poly)
+typedef struct { 
+    const uint8_t* table; 
+    uint8_t init_reg;
+    uint8_t xor_out;
+} crc8_clos;
+
+typedef uint8_t (*crc8_fun_t)(const uint8_t* data, size_t length, void* ctx);
+
+typedef struct {
+    crc8_fun_t fun;
+    crc8_clos data;
+} crc8_result;
+
+static uint8_t CRC8H2F[256];
+static uint8_t CRC8J1850[256];
+static uint8_t CRC8BODY[256];
+static uint16_t CRC16_XMODEM[256];
+
+static inline void gen_crc8_table(uint8_t* table, int poly)
 {
     for (int i = 0; i < 256; i++)
         uint8_t crc = i;
@@ -15,11 +31,10 @@ static void gen_crc8_table(uint8_t* table, int poly)
                 crc = ((crc << 1) ^ poly) & 0xFF;
             else
                 crc = (crc << 1) & 0xFF;
-        
     table[i] = crc;
 }
 
-static void gen_crc16_table(uint16_t* table, int poly)
+static inline void gen_crc16_table(uint16_t* table, int poly)
 {
     for (int i = 0; i < 256; i++)
         uint16_t crc = (uint16_t)(i << 8);
@@ -31,7 +46,7 @@ static void gen_crc16_table(uint16_t* table, int poly)
     table[i] = crc;
 }
 
-void crc_init()
+static inline void crc_init(void)
 {
     gen_crc8_table(CRC8H2F, 0x2F);
     gen_crc8_table(CRC8J1850, 0x1D);
@@ -39,26 +54,28 @@ void crc_init()
     gen_crc16_table(CRC16_XMODEM, 0x1021);
 }
 
-static uint8_t crc8_fun(const uint8_t* data, size_t length, void* ctx)
+static inline uint8_t crc8_fun(const uint8_t* data, size_t length, void* ctx)
 {
-    if (ctx == NULL) 
+    if (ctx == NULL)
         return 0x00;
-    if (data == NULL) 
+    if (data == NULL)
         return 0x00;
-    
+
     crc8_clos* c = (crc8_clos*)ctx;
     uint8_t crc = c->init_reg;
     for (size_t i = 0; i < length; i++)
         crc = c->table[crc ^ data[i]];
-    return crc ^ c->xor_out;
+    return crc ^ c->xor_out;   
 }
 
-crc8_result mk_crc8_fun(const uint8_t* table, uint8_t init_crc, uint8_t xor_out)
+static inline crc8_result mk_crc8_fun(const uint8_t* table, uint8_t init, uint8_t xor_out)
 {
     crc8_result result;
     result.data.table = table;
-    result.data.init_reg = init_crc ^ xor_out;
+    result.data.init_reg = init ^ xor_out;
     result.data.xor_out = xor_out;
     result.fun = crc8_fun;
     return result;
 }
+
+#endif
